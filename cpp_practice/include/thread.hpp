c10d2fcb6a2ecad73e47e03bde3b21eceb586e08
Dav1nGen <davicheng1114@gmail.com>
2024-12-16 18:29:01 +0800

    Dav1nGen

--- cpp_practice/include/thread.hpp
+++ cpp_practice/include/thread.hpp
@@ -2,30 +2,49 @@
 #define CPP_PRACTICE_THREAD_HPP
 
 #include <iostream>
+#include <string>
+#include <assert.h>
 
 #include <thread>
-#include <opencv4/opencv.hpp>
+#include <opencv4/opencv2/opencv.hpp>
+#include <opencv4/opencv2/core.hpp>
+#include <opencv4/opencv2/core/persistence.hpp>
 
 namespace thread
 {
-    void Get_parameters()
+    class FileReader
     {
-        cv::FileStorage fs("../data/data.xml",cv::FileStorage::READ);
-        if(!fs.isOpened())
+    public:
+        FileReader(std::string file_path) : _file_path(file_path)
         {
-            std::cout << "Failed to open file" << "\n";
-        }
-        else
-        {
+            cv::FileStorage file_storage(_file_path, cv::FileStorage::READ);
+            assert(file_storage.isOpened());
             std::cout << "File opened successfully" << "\n";
+
+            this->fs = file_storage;
         }
-    }
+        cv::FileStorage fs;
+
+    private:
+        std::string _file_path;
+    };
 
     void Test1()
     {
+        int width;
+        int height;
+        int fps;
         std::thread t([]()
                       { std::cout << "Hello from thread!" << "\n"; });
         t.join();
+        FileReader fr("../../../../config/config.yaml");
+        fr.fs["width"] >> width;
+        fr.fs["height"] >> height;
+        fr.fs["fps"] >> fps;
+
+        std::cout << "width: " << width << "\n"
+                  << "height: " << height << "\n"
+                  << "fps: " << fps << "\n";
     }
 }
 
